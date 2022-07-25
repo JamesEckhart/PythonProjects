68162fea3a48f212ede01dc32c5a219f8663b4e1
James Eckhart <james@jameseckhartjr.com>
2022-07-25 15:54:18 -0600

    global local variables, again and again!! I love Git :)

--- Local Global.py
+++ Local Global.py
@@ -4,9 +4,9 @@ print(f)
 
 #Global vs. local variables in fuctions
 def someFunction():
-    f = 'Testing 1...2...3...' #Local variable inside the function, assumes local scope
+    f = 'Testing 1...2...3...4...' #Local variable inside the function, assumes local scope
     print(f)
 
 someFunction()
 
-print(f) #global varable, local variable was destroyed
+print(f) #global varable, local variable was destroyed!
