49a27241241f18932e3fa5ba482246bcbb3f3576
James Eckhart <james@jameseckhartjr.com>
2022-07-25 15:58:03 -0600

    global local variables, again and again!! I love Git :)

--- Local Global.py
+++ Local Global.py
@@ -4,7 +4,7 @@ print(f)
 
 #Global vs. local variables in fuctions
 def someFunction():
-    f = 'Testing 1...2...3...4...' #Local variable inside the function, assumes local scope
+    f = 'Testing 1...2...3...4...5...' #Local variable inside the function, assumes local scope
     print(f)
 
 someFunction()
