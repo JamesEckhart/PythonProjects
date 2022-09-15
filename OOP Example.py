61291a492bc22bc583a948751a588f77256f5918
James Eckhart <james@jameseckhartjr.com>
2022-09-14 20:04:33 -0600

    New commit...

--- OOP Example.py
+++ OOP Example.py
@@ -2,5 +2,5 @@ kirk = ["James Kirk", 34, "Captain", 2265]
 spock = {"Spock", 35, "Science Officer", 2254}
 mccoy = {"Leonard McCoy", "Chief Medical Officer", 2266}
 
-y = kirk[0]
-print(y)
+x = kirk[0]
+print(x)
