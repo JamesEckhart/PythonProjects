e3d61864c00d507305c855af1e26ca5c535127d6
James Eckhart <james@jameseckhartjr.com>
2023-07-14 12:42:37 -0600

    Variable updated.

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,15 +1,15 @@
-def insertionSort(nlist):
-   for index in range(1,len(nlist)):
+def insertionSort(list):
+   for index in range(1,len(list)):
 
-     currentvalue = nlist[index]
+     currentvalue = list[index]
      position = index
 
-     while position>0 and nlist[position-1]>currentvalue:
-         nlist[position]=nlist[position-1]
+     while position>0 and list[position-1]>currentvalue:
+         list[position]=list[position-1]
          position = position-1
 
-     nlist[position]=currentvalue
+     list[position]=currentvalue
 
-nlist = [14,46,43,27,57,41,45,21,70]
-insertionSort(nlist)
-print(nlist)
+list = [14,46,43,27,57,41,45,21,70]
+insertionSort(list)
+print(list)
